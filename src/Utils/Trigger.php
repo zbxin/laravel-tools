7b18bc136073ee2d2eb890ff2b05ddaca15bafe0
古俊杰 <ilovintit@gmail.com>
2018-10-15 16:51:00 +0800

    增加事件触发器辅助工具

---
+++ src/Utils/Trigger.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ZhiEq\Utils;
+
+use Illuminate\Database\Connection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
+
+class Trigger
+{
+    /**
+     * @param $event
+     * @return bool|mixed
+     */
+
+    public static function eventResult($event)
+    {
+        $eventResult = event($event);
+        if (Event::getListeners(get_class($event)) !== count($eventResult)) {
+            return false;
+        }
+        $results = array_filter($eventResult, function ($result) {
+            return $result !== true && $result !== null;
+        });
+        return empty($results) ? true : array_pop($results);
+    }
+
+    /**
+     * @param $event
+     * @return mixed
+     */
+
+    public static function eventWithTransaction($event)
+    {
+        return DB::transaction(function (Connection $db) use ($event) {
+            $result = self::eventResult($event);
+            if ($result === false) {
+                $db->rollBack();
+            }
+            return $result;
+        });
+    }
+}
